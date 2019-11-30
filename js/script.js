75f5848978b6c1661712223070847115b9137aec
martin <martinaguirre08@gmail.com>
2019-11-30 16:28:39 -0300

    agregar y borrar incluido close #2

--- js/script.js
+++ js/script.js
@@ -12,6 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     MostrarTabla();
 
+    let btnBorrar = document.getElementById('borrar');
+    btnBorrar.addEventListener("click", borrarTabla);
+
+    let btnEnviar = document.getElementById('enviarSugerencia');
+    btnEnviar.addEventListener("click", nuevaMaqueta);
+
     function MostrarTabla() {
         cuerpoTabla.innerHTML = "";
         for (let i = 0; i < tablaSugerencias.length; i++) {
@@ -22,4 +28,32 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         
     };
+
+    function nuevaMaqueta(e) {
+        e.preventDefault();
+        let autoIntroducidoPorUsuario = document.getElementById('Auto').value;
+        let añoIntroducidoPorUsuario = document.getElementById('Año').value;
+        let verificacionAuto = autoIntroducidoPorUsuario.trim();
+
+        if (añoIntroducidoPorUsuario > 1886 && añoIntroducidoPorUsuario < 2020 && verificacionAuto != "") {
+
+
+
+            let nuevoAuto = {
+                "Maqueta": autoIntroducidoPorUsuario,
+                "Año": añoIntroducidoPorUsuario
+            };
+
+            tablaSugerencias.push(nuevoAuto);
+        } else {
+            alert("No ingresó un dato válido");
+        }
+
+        MostrarTabla();
+    };
+
+    function borrarTabla() {
+        tablaSugerencias = [];
+        cuerpoTabla.innerHTML = "";
+    }
 });
