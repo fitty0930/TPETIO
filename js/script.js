1cebdd883a3226284f354c728ad6f0d6183f37a5
martin <martinaguirre08@gmail.com>
2019-11-29 23:53:11 -0300

    agregado de una tabla dinamica js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     "use strict";
+
+    let cuerpoTabla = document.querySelector("#autos-tabla");
+
+    let tablaSugerencias = [
+        { "Maqueta": "Ford Falcon", "Año": "1962" },
+        { "Maqueta": "Chevrolet Chevy", "Año": "1971" },
+        { "Maqueta": "GMC Coach TDH 3610", "Año": "1948" }
+    ];
+
+    MostrarTabla();
+
+    function MostrarTabla() {
+        cuerpoTabla.innerHTML = "";
+        for (let i = 0; i < tablaSugerencias.length; i++) {
+            cuerpoTabla.innerHTML += "<tr>" +
+                "<td>" + tablaSugerencias[i].Maqueta + "</td>" +
+                "<td>" + tablaSugerencias[i].Año + "</td>" +
+                "</tr>";
+        }
+        
+    };
 });
