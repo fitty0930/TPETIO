9bcbd7930729000dfca2a63a36eac6554d410b23
martin <martinaguirre08@gmail.com>
2019-11-30 16:48:01 -0300

    agregado los filtros close #3

--- js/script.js
+++ js/script.js
@@ -18,6 +18,16 @@ document.addEventListener("DOMContentLoaded", function () {
     let btnEnviar = document.getElementById('enviarSugerencia');
     btnEnviar.addEventListener("click", nuevaMaqueta);
 
+    let textbuscar = document.getElementById("buscar");
+    textbuscar.onkeyup = function () {
+        buscar();
+    }
+
+    let textresaltar = document.getElementById("resaltar");
+    textresaltar.onkeyup = function () {
+        resaltar();
+    }
+
     function MostrarTabla() {
         cuerpoTabla.innerHTML = "";
         for (let i = 0; i < tablaSugerencias.length; i++) {
@@ -55,5 +65,34 @@ document.addEventListener("DOMContentLoaded", function () {
     function borrarTabla() {
         tablaSugerencias = [];
         cuerpoTabla.innerHTML = "";
+    }
+
+    function buscar() {
+        let valorabuscar = document.getElementById('buscar').value.toLowerCase().trim();
+        let tabla_tr = document.getElementsByTagName("tbody")[0].rows;
+        for (let i = 0; i < tabla_tr.length; i++) {
+            let tr = tabla_tr[i];
+            let textotr = tr.innerText.toLowerCase();
+            tr.className = (textotr.indexOf(valorabuscar) != -1) ? "mostrar" : "ocultar";
+            // operador ternario
+        }
+    }
+
+    function resaltar() {
+        let valoraresaltar = document.getElementById('resaltar').value.toLowerCase().trim();
+        let tabla_tr = document.getElementsByTagName("tbody")[0].rows;
+        if (valoraresaltar != "") {
+            for (let i = 0; i < tabla_tr.length; i++) {
+                let tr = tabla_tr[i];
+                let textotr = tr.innerText.toLowerCase();
+                tr.className = (textotr.indexOf(valoraresaltar) != -1) ? "resaltado" : "noresaltado";
+                // operador ternario
+            }
+        } else {
+            for (let i = 0; i < tabla_tr.length; i++) {
+                let tr = tabla_tr[i];
+                tr.classList.remove("resaltado");
+            }
+        }
     }
 });
